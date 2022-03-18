078bcf1a90d583d08047ccbb34c4b61d9926df25
Irina Bystrova <66327312+IrenBys@users.noreply.github.com>
2022-03-18 22:41:28 +0300

    1 - UPDATED PYTHON SCRIPTS
     - Created tools class
     - Updated tne main function

--- python/port.py
+++ python/port.py
@@ -2,54 +2,11 @@
 ## This script is used to send and receice data between two COM ports ##
 ########################################################################
 import sys
-import serial
-from serial.tools import list_ports
-
-def find_ftdi_ports() -> list:
-    ftdi_ports = []
-    ports = list_ports.comports()
-    for port in ports:
-        serial_num = str(port.serial_number)            # it's necessary, cause the var port.serial_number is NoneType
-        if port.vid == 1027 and serial_num[0] == "F":   # add only FTDI devices 
-            ftdi_ports.append(port.device)
-    # if no one FTDI device was found                                                      
-    if not ftdi_ports:
-        print("Couldn't find any FTDI port") 
-        return None  
-    print(ftdi_ports)      
-    return ftdi_ports
-    
-
-def open_ftdi_ports() -> list:
-    # generate the empty list of opened serial ports
-    serial_list = []
-    # get a list of avaible ports connected to FTDIs
-    ftdi_ports = find_ftdi_ports()
-    # open ports connected to FTDIs
-    if ftdi_ports is not None: 
-        try:
-            for port_num in ftdi_ports:
-                serial_port = serial.Serial(
-                    port=port_num,
-                    baudrate=115200,
-                    bytesize=serial.EIGHTBITS,
-                    parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_ONE,
-                    timeout=1
-                )
-                # add opened port to the list 
-                serial_list.append(serial_port)
-                print(f"The {serial_port.port} is opened!")
-                return serial_list 
-
-        except serial.SerialException as e:
-            print(e)
-            
-    return None           
+from port_tools import tools
 
 
 def main():
-    serial_list = open_ftdi_ports()    
+    serial_list = tools().open_ftdi_ports()   
     if serial_list is not None:
         # the first port from the list is always selected for sending data
         print(f"The port to send data: {serial_list[0].port}")
--- python/port_tools.py
+++ python/port_tools.py
@@ -1,57 +1,45 @@
 import serial
 from serial.tools import list_ports
-import traceback
-
-# ser = serial.Serial()
- 
-# ftdi_ports = []
-# ports = list_ports.comports()
-# for port in ports:
-#     #if port.vid == 1027:
-#         #ftdi_ports.append(port.device)
-#     #print(port.vid)
-#     #print(port.device)
-#     #print(port.name)
-#     #print(port.description)
-#     #print(port.hwid)
-#     #print(list(str(port.pid)))
-#     print(list(str(port.serial_number)))
-#     #print(port.product)
-#     #print(port.interface)
-# # print(f'Found {len(ftdi_ports)} FTDI ports')
-
-# def __find_ftdi_ports():
-#     ftdi_ports = []
-#     ports = list_ports.comports()
-#     for port in ports:
-#         serial_num = str(port.serial_number)
-#         if port.vid == 1027 and serial_num[0] == "F":   # add only FTDI devices 
-#             ftdi_ports.append(port.device)                    
-#     if ftdi_ports == []:
-#         print("Couldn't find any FTDI port")            
-#         return None
-#     return ftdi_ports
 
 
-try:
-  ser_port = serial.Serial( # set parameters, in fact use your own :-)
-    port="COM5",
-    baudrate=9600,
-    bytesize=serial.SEVENBITS,
-    parity=serial.PARITY_EVEN,
-    stopbits=serial.STOPBITS_ONE
-  )
-  ser_port.isOpen() # try to open port, if possible print message and proceed with 'while True:'
-  print ("port is opened!")
-
-except Exception:
-    traceback.print_exc()
-
-while True: 
-    pass
-    # do something...
-            
-
+class tools:
     
+    def find_ftdi_ports(self) -> list:
+        self.ftdi_ports = []
+        self.ports = list_ports.comports()
+        for port in self.ports:
+            if port.vid == 1027:
+                # add only FTDI devices    
+                self.ftdi_ports.append(port.device)
+        # if no one FTDI device was found                                                      
+        if not self.ftdi_ports:
+            print("Couldn't find any FTDI port") 
+            return None  
+        return self.ftdi_ports
+    
+    def open_ftdi_ports(self) -> list:
+        # generate the empty list of opened serial ports
+        serial_list = []
+        # get a list of avaible ports connected to FTDIs
+        ftdi_ports = tools().find_ftdi_ports()
+        # open ports connected to FTDIs
+        if ftdi_ports is not None: 
+            try:
+                for port_num in ftdi_ports:
+                    serial_port = serial.Serial(
+                        port=port_num,
+                        baudrate=115200,
+                        bytesize=serial.EIGHTBITS,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE,
+                        timeout=1
+                    )
+                    # add opened port to the list 
+                    serial_list.append(serial_port)
+                    print(f"The {serial_port.port} is opened!")
+                    return serial_list 
 
-
+            except serial.SerialException as e:
+                print(e)
+                
+        return None
